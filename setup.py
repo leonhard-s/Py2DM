4355a04e3bd5f678f74d614d6367426bf9ee4171
Leonhard S <leonhard-sei@outlook.com>
2020-06-11 20:26:12 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import setuptools
+
+# Read long description
+readme_dir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(readme_dir, 'README.md')) as readme:
+    long_description = readme.read()
+
+setuptools.setup(name='Py2DM',
+                 version='0.1.0a',
+                 description='Read and write 2DM mesh files',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/leonhard-s/Py2DM',
+                 author='Leonhard S.',
+                 classifiers=['Development Status :: 3 - Alpha',
+                              'Intended Audience :: Developers',
+                              'License :: OSI Approved :: MIT License',
+                              'Programming Language :: Python :: 3',
+                              'Programming Language :: Python :: 3.7',
+                              'Programming Language :: Python :: 3 :: Only'],
+                 keywords='mesh 2dm')
