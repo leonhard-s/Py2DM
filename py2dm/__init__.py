16931629c66653cb727515e483b08b0cf6669796
Leonhard S <leonhard-sei@outlook.com>
2020-11-07 19:00:50 +0100

    Conform init format

--- py2dm/__init__.py
+++ py2dm/__init__.py
@@ -6,6 +6,7 @@ detailed usage instructions and to provide feedback.
 For additional information on the 2DM format, refer to the 2DM file
 format specification at
 https://www.xmswiki.com/wiki/SMS:2D_Mesh_Files_*.2dm.
+
 """
 
 from . import errors
@@ -15,9 +16,23 @@ from .entities import (Element, Element2L, Element3L, Element3T, Element4Q,
 from .read import Reader
 from .write import Writer
 
-__all__ = ['Element', 'Element2L', 'Element3L', 'Element3T', 'Element4Q',
-           'Element6T', 'Element8Q', 'Element9Q', 'LinearElement', 'Node',
-           'NodeString', 'QuadrilateralElement', 'Reader', 'TriangularElement',
-           'Writer']
+__all__ = [
+    'Element',
+    'Element2L',
+    'Element3L',
+    'Element3T',
+    'Element4Q',
+    'Element6T',
+    'Element8Q',
+    'Element9Q',
+    'errors',
+    'LinearElement',
+    'Node',
+    'NodeString',
+    'QuadrilateralElement',
+    'Reader',
+    'TriangularElement',
+    'Writer'
+]
 
 __version__ = '0.1.1'
