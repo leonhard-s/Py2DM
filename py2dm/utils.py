9c7ce86f1aeef726884756e690bba08299e8c9ec
Leonhard S <leonhard-sei@outlook.com>
2021-03-03 11:32:22 +0100

    Remove unused utilities

--- py2dm/utils.py
+++ py2dm/utils.py
@@ -1,7 +1,7 @@
 """Shared utilities used for reading and writing 2DM files."""
 
 import contextlib
-from typing import Iterator, SupportsFloat
+from typing import SupportsFloat
 
 from .types import MaterialIndex
 
@@ -20,21 +20,6 @@ def cast_matid(value: str) -> MaterialIndex:
     with contextlib.suppress(ValueError):
         return float(value)
     raise TypeError(f'Unable to convert string literal to MATID: {value}')
-
-
-def clean_line(line: str) -> str:
-    """Return a cleaned version of the input line's contents.
-
-    This means removing any leading and trailing whitespace, as well as
-    discarding any trailing inline comments.
-
-    :param line: The input line to process
-    :type line: str
-    :return: The cleaned contents of the line
-    :rtype: str
-    """
-    data = line.split('#', 1)[0]
-    return data.strip()
 
 
 def format_float(value: SupportsFloat, *, decimals: int = 8) -> str:
@@ -71,27 +56,3 @@ def format_matid(value: MaterialIndex, *, decimals: int = 8) -> str:
     """
     return (str(value) if isinstance(value, int)
             else format_float(value, decimals=decimals))
-
-
-def next_line(iterator: Iterator[str]) -> str:
-    """Return the next valid 2DM line.
-
-    This will jump over any empty lines, as well as strip out any
-    comments.
-
-    This will return an empty string only if the iterator is exhausted
-    while attempting to return a valid line.
-
-    :param iterator: The iterator to use for traversal of the file
-    :type line: Iterator [str]
-    :return: The next non-empty line
-    :rtype: str
-    """
-    while True:
-        try:
-            line = next(iterator)
-        except StopIteration:
-            return ''
-        line = clean_line(line)
-        if line:
-            return line
