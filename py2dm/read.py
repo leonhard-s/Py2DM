86f588c045d02351bc733f02f48dd17a5cfaad2d
Leonhard S <leonhard-sei@outlook.com>
2021-03-03 13:56:43 +0100

    Flatten multi-reader approach
    Removed the ABC and MemoryReader implementation to avoid the inherent overhead and complexity. This leaves the reader in a broken state, this commit is not usable.

--- py2dm/read.py
+++ py2dm/read.py
@@ -7,10 +7,8 @@ for details.
 
 """
 
-import abc
-import enum
 from types import TracebackType
-from typing import Any, ClassVar, Iterator, List, Optional, Type, TypeVar
+from typing import Any, ClassVar, Iterator, List, Optional, Type
 
 from .entities import Element, Node, NodeString
 
@@ -24,12 +22,9 @@ except ModuleNotFoundError as err:  # pragma: no cover
         raise err from err
 
 __all__ = [
-    'MemoryReader',
-    'Reader',
-    'ReadMode'
+    'Reader'
 ]
 
-ReaderT = TypeVar('ReaderT', bound='Reader')
 
 # A list of all tags that are considered elements
 _ELEMENTS = [
@@ -43,93 +38,8 @@ _ELEMENTS = [
 ]
 
 
-class ReadMode(enum.Enum):
-    """Enumerator for different file reading strategies.
-
-    These modes alter the way the :class:`py2dm.Reader` class accesses
-    the underlying file data. Depending on the application and mesh
-    size, specifying a different file reading strategy can lead to
-    significant improvements or degradations in performance.
-
-    For more information on which modes are better suited for a given
-    application, refer to the [Optimisation] section of the project
-    documentation.
-
-    Attributes:
-        MEMORY: The entire mesh is loaded at once, and the object
-            representations of all nodes and elements are stored in
-            memory.
-
-            This minimises random access times, but also consumes a lot
-            of memory in the process, which in turn can lead to poor
-            system performance or instability.
-
-        BATCHED: Mesh data is retrieved in batches of a given size as
-            specified by :class:`py2dm.BatchedReader` class's
-            `batch_size` argument.
-
-            The beginning of each batch is cached, allowing for
-            retrieval of a given node or element by matching its ID to
-            its associated batch. Separate batches are created for
-            nodes and elements.
-
-            This mode provides a good balance of random read speeds and
-            memory usage, particularly when there is some correlation
-            between access order and entity IDs, as entities with
-            similar IDs are likely to be stored in the same batch.
-
-        LAZY: This mode keeps the entire mesh on disk and reads single
-            lines of data as required. When retrieving objects by ID,
-            a binary-search is performed to identify the corresponding
-            line of data.
-
-            This mode minimises memory usage and is recommended for
-            extremely large files, but is heavily bottlenecked by the
-            seek and read speeds of the storage medium used.
-
-    """
-
-    MEMORY = 10
-    BATCHED = 20
-    LAZY = 30
-
-
-class Reader(metaclass=abc.ABCMeta):
-    """Reader interface specification and class factory.
-
-    This class acts as the ABC defining the endpoints available for all
-    of its subclasses regardless of implementation.
-
-    It also acts as a factory method for retrieving the corresponding
-    subclass by mode:
-
-    .. code-block:: python
-
-        >>> reader = py2dm.Reader('mesh.2dm', mode=ReadMode.LAZY)
-        >>> type(reader)
-        <class 'py2dm.read.LazyReader'>
-
-    Subclasses are only meant to implement the abstract methods of this
-    ABC. The only tags they are responsible for are nodes, elements and
-    node strings. Everything else (metadata, mesh names, node or
-    element count, etc.) is handled by the :class:`py2dm.Reader` class.
-
-    .. note::
-
-        The abstract methods here are abstract because they raise a
-        ``NotImplementedError`` in their default implementation. They
-        do not use the :meth:`abc.abstractmethod()` decorators as this
-        upsets linters due to the factory-like usage of this base
-        class.
-
-        This is done for compatibility with the previous version's
-        syntax but has been deprecated.
-
-    """
-
-    # This attribute is used by subclasses to indicate which ReadMode they
-    # correspond to
-    _mode: ClassVar[Optional[ReadMode]] = None
+class Reader:
+    """Reader interface specification and class factory."""
 
     def __init__(self, filepath: str, validate: bool = True,
                  **kwargs: Any) -> None:
@@ -147,50 +57,23 @@ class Reader(metaclass=abc.ABCMeta):
                 Defaults to ``True``.
 
         """
-        _ = kwargs.pop('mode', None)
-        if kwargs:
-            kwarg = next(iter(kwargs))
-            raise TypeError(
-                f'__init__() got an unexpected keyword argument \'{kwarg}\'')
         self._filepath = filepath
         if validate:
             self._validate()
-
-    def __new__(cls: Type[ReaderT], *args: Any, mode: ReadMode = ReadMode.MEMORY,
-                **kwargs: Any) -> ReaderT:
-        """Return a new :class:`py2dm.Reader` instance.
-
-        If `mode` has been set to a value other than ``None``, this
-        method will act as a factory method dispensing the appropriate
-        subclass of the :class:`py2dm.Reader` interface.
-
-        Arguments:
-            mode (optional): The read mode to use. This controls which
-                subclass of :class:`py2dm.Reader` will be returned.
-                Defaults to :attr:`py2dm.ReadMode.MEMORY`.
-
-        Raises:
-            NotImplementedError: Raised if the given `mode` does not
-                match any subclass of :class:`py2dm.Reader`.
-
-        Returns:
-            A subclass of :class:`py2dm.Reader` if the `mode` argument
-            has been set to a value other than ``None``, or a regular
-            object instance as per the default implementation of
-            :meth:`object.__new__()`.
-
-        """
-        if cls == Reader:
-            for subclass in cls.__subclasses__():
-                if subclass._mode == mode:
-                    return subclass(*args, **kwargs)
-            raise NotImplementedError(
-                f'No subclass registered for read mode \'{mode}\'')
-        # NOTE: A mode of None is reserved to allow propagation of this call to
-        # the default object constructor, since this method has to act work as
-        # both the factory and as part of the regular object instantiation MRO.
-        instance: ReaderT = super().__new__(cls)
-        return instance
+        # Load all searchable entities into memory
+        self._cache_nodes: List[Node] = [
+            Node.parse_line(l) for l in self._filter_lines('ND')]
+        self._cache_elements: List[Element] = [
+            _element_factory(l[0]).parse_line(l)
+            for l in self._filter_lines(*_ELEMENTS)]
+        # Node strings are special and require multiline parsing
+        self._cache_node_strings: List[NodeString] = []
+        with open(self._filepath, 'r') as file_:
+            for line in iter(file_):
+                if line.startswith('NS'):
+                    node_string, is_done = NodeString.parse_line(line.split())
+                    if is_done:
+                        self._cache_node_strings.append(node_string)
 
     def __enter__(self) -> 'Reader':
         """Enter the context manager.
@@ -211,6 +94,7 @@ class Reader(metaclass=abc.ABCMeta):
         This will never suppress any exceptions.
 
         """
+        _ = exc_type, exc_value, exc_tb
         self.close()
         return False
 
@@ -272,17 +156,17 @@ class Reader(metaclass=abc.ABCMeta):
     @property
     def num_elements(self) -> int:
         """Return the number of elements in the mesh."""
-        raise NotImplementedError  # pragma: no cover
+        return len(self._cache_elements)
 
     @property
     def num_nodes(self) -> int:
         """Return the number of nodes in the mesh."""
-        raise NotImplementedError  # pragma: no cover
+        return len(self._cache_nodes)
 
     @property
     def num_node_strings(self) -> int:
         """Return the number of node strings in the mesh."""
-        raise NotImplementedError  # pragma: no cover
+        return len(self._cache_node_strings)
 
     def close(self) -> None:
         """Close the mesh reader.
@@ -297,193 +181,6 @@ class Reader(metaclass=abc.ABCMeta):
             via the context manager.
 
         """
-
-    def element(self, id_: int) -> Element:
-        """Return a mesh element by its unique ID.
-
-        Arguments:
-            id_: The ID of the element to return.
-
-        Raises:
-            IndexError: Raised if the given `id_` is negative or
-                exceeds the number of elements in the mesh.
-
-        Returns:
-            The element matching the given ID.
-
-        """
-        raise NotImplementedError  # pragma: no cover
-
-    def node(self, id_: int) -> Node:
-        """Return a mesh node by its unique ID.
-
-        Arguments:
-            id_: The ID of the node to return.
-
-        Raises:
-            IndexError: Raised if the given `id_` is negative or
-                exceeds the number of nodes in the mesh.
-
-        Returns:
-            The node matching the given ID.
-
-        """
-        raise NotImplementedError  # pragma: no cover
-
-    def iter_elements(self, start: int = 1,
-                      end: int = -1) -> Iterator[Element]:
-        """Iterate over the mesh elements.
-
-        Arguments:
-            start (optional): The starting element ID. Defaults to
-                ``1``.
-
-            end (optional): The end element ID (exclusive). If
-                negative, continues until the list of elements is
-                exhausted. Defaults to ``-1``.
-
-        Raises:
-            IndexError: Raised if the `start` ID is less than ``1``, or
-                if the `end` ID is less than or equal to the `start`
-                ID, or if either of the IDs exceeds the number of
-                elements in the mesh.
-
-        Yields:
-            Mesh elements from the given range of IDs.
-
-        """
-        raise NotImplementedError  # pragma: no cover
-
-    def iter_nodes(self, start: int = 1, end: int = -1) -> Iterator[Node]:
-        """Iterate over the mesh elements.
-
-        If the `end` ID is less than the `start` ID, the IDs will be
-        traversed in reverse order.
-
-        Arguments:
-            start (optional): The starting node ID. Must be greater
-                than or equal to ``1``. Defaults to ``1``.
-
-            end (optional): The end node ID (exclusive). If negative,
-                continues until the list of nodes is exhausted.
-                Defaults to ``-1``.
-
-        Raises:
-            IndexError: Raised if the `start` ID is less than ``1``, or
-                if the `end` ID is less than or equal to the `start`
-                ID, or if either of the IDs exceeds the number of
-                nodes in the mesh.
-
-        Yields:
-            Mesh nodes from the given range of IDs.
-
-        """
-        raise NotImplementedError  # pragma: no cover
-
-    def iter_node_strings(self) -> Iterator[NodeString]:
-        """Iterate over the mesh node strings.
-
-        Yields:
-            Mesh node strings in order of definition.
-
-        """
-        raise NotImplementedError  # pragma: no cover
-
-    def _validate(self) -> None:
-        """Check the mesh file for issues and incompatibilities."""
-        with open(self._filepath) as file_:
-            for line in file_:
-                if line.startswith(('ND', *_ELEMENTS)):
-                    _, id_str, *_ = line.split(maxsplit=2)
-                    if int(id_str) == 0:
-                        raise RuntimeError(
-                            'Zero-indexed node strings are not supported. '
-                            'See the documentation on ways to convert your '
-                            'meshes to a compatible format.')
-
-
-class MemoryReader(Reader):
-    """In-memory reader for 2DM mesh files.
-
-    This :class:`py2dm.Reader` subclass loads the entire mesh upon
-    initialisation and stores the object representations of its nodes,
-    elements, and node strings in memory.
-
-    This is not feasible for large meshes due to the high memory usage,
-    but results in the shortest random access times.
-
-    """
-
-    _mode = ReadMode.MEMORY
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        """Initialise the mesh reader.
-
-        This opens the underlying file and preloads metadata for the
-        mesh.
-
-        Arguments:
-            *args: Arguments for :meth:`py2dm.Reader.__init__()`.
-
-            **kwargs: Keyword arguments for
-                :meth:`py2dm.Reader.__init__()`.
-
-        """
-        super().__init__(*args, **kwargs)
-        # Load all searchable entities into memory
-        self._cache_nodes: List[Node] = [
-            Node.parse_line(l) for l in self._filter_lines('ND')]
-        self._cache_elements: List[Element] = [
-            _element_factory(l[0]).parse_line(l)
-            for l in self._filter_lines(*_ELEMENTS)]
-        # Node strings are special and require multiline parsing
-        self._cache_node_strings: List[NodeString] = []
-        with open(self._filepath, 'r') as file_:
-            for line in iter(file_):
-                if line.startswith('NS'):
-                    node_string, is_done = NodeString.parse_line(line.split())
-                    if is_done:
-                        self._cache_node_strings.append(node_string)
-
-    @property
-    def num_nodes(self) -> int:
-        """Return the number of elements in the mesh."""
-        return len(self._cache_nodes)
-
-    @property
-    def num_elements(self) -> int:
-        """Return the number of nodes in the mesh."""
-        return len(self._cache_elements)
-
-    @property
-    def num_node_strings(self) -> int:
-        """Return the number of node strings in the mesh."""
-        return len(self._cache_node_strings)
-
-    def _filter_lines(self, card: str, *args: str) -> Iterator[List[str]]:
-        """Filter the mesh's lines by their card.
-
-        This iterates over the entire file, returning only those lines
-        matching the given card.
-
-        This also filters out any comments appended to the end of a
-        line.
-
-        Arguments:
-            card: The card to match lines against.
-
-            *args: Additional cards whos lines will be returned.
-
-        Yields:
-            A list of whitespace-separated words of the matching line.
-
-        """
-        valid_cards = card, *args
-        with open(self._filepath, 'r') as file_:
-            for line in file_:
-                if line.startswith(valid_cards):
-                    line, *_ = line.split('#', maxsplit=1)
-                    yield line.split()
 
     def element(self, id_: int) -> Element:
         """Return a mesh element by its unique ID.
@@ -605,6 +302,43 @@ class MemoryReader(Reader):
 
         """
         return iter(self._cache_node_strings)
+
+    def _filter_lines(self, card: str, *args: str) -> Iterator[List[str]]:
+        """Filter the mesh's lines by their card.
+
+        This iterates over the entire file, returning only those lines
+        matching the given card.
+
+        This also filters out any comments appended to the end of a
+        line.
+
+        Arguments:
+            card: The card to match lines against.
+
+            *args: Additional cards whos lines will be returned.
+
+        Yields:
+            A list of whitespace-separated words of the matching line.
+
+        """
+        valid_cards = card, *args
+        with open(self._filepath, 'r') as file_:
+            for line in file_:
+                if line.startswith(valid_cards):
+                    line, *_ = line.split('#', maxsplit=1)
+                    yield line.split()
+
+    def _validate(self) -> None:
+        """Check the mesh file for issues and incompatibilities."""
+        with open(self._filepath) as file_:
+            for line in file_:
+                if line.startswith(('ND', *_ELEMENTS)):
+                    _, id_str, *_ = line.split(maxsplit=2)
+                    if int(id_str) == 0:
+                        raise RuntimeError(
+                            'Zero-indexed node strings are not supported. '
+                            'See the documentation on ways to convert your '
+                            'meshes to a compatible format.')
 
 
 def _element_factory(card: str) -> Type[Element]:
